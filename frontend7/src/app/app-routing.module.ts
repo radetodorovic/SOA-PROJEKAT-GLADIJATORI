dcc58f5ac24583e29e8ac44c2a4ec66e98a6744a
BlagojePanic <blagojepanic777@gmail.com>
2026-04-08 18:01:28 +0200

    Cetvrta funkcionalnost

--- frontend7/src/app/app-routing.module.ts
+++ frontend7/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthComponent } from './pages/auth/auth.component';
 import { AdminUsersComponent } from './pages/admin-users/admin-users.component';
+import { MyProfileComponent } from './pages/my-profile/my-profile.component';
 
 const routes: Routes = [
   { path: '', component: AuthComponent },
   { path: 'auth', component: AuthComponent },
   { path: 'admin/users', component: AdminUsersComponent },
+  { path: 'my-profile', component: MyProfileComponent },
   { path: '**', redirectTo: '' }
 ];
 
--- frontend7/src/app/app.module.ts
+++ frontend7/src/app/app.module.ts
@@ -1,24 +1,27 @@
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthComponent } from './pages/auth/auth.component';
 import { AdminUsersComponent } from './pages/admin-users/admin-users.component';
+import { MyProfileComponent } from './pages/my-profile/my-profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AuthComponent,
-    AdminUsersComponent
+    AdminUsersComponent,
+    MyProfileComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
